1e04215ec5a7e1dc224eb05ba542a660b68a983d
Arinzelight <arinzelight2@gmail.com>
2024-01-05 05:43:47 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, displays the size of the body of the response
-curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
+
+# Check if the URL parameter is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Use curl to send a request to the provided URL and display the size of the response body
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
 
