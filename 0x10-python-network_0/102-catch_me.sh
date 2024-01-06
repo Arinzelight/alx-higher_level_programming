013329d76c25dfabd5da860b9b631735662cbde3
Arinzelight <arinzelight2@gmail.com>
2024-01-05 17:15:39 -0800

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-# Make a POST request to 0.0.0.0:5000/catch_me with a custom User-Agent header and the data "You got me!"
-curl -sX PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" -H "Content-Type: application/json" -d '{"You got me!"}' http://0.0.0.0:5000/catch_me
-
+# script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sX PUT -d -L 'user_id=98' -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
