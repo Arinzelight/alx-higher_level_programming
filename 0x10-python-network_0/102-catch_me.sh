edf9493d25cf153e2abac3b15a64e6ddbe481c98
Arinzelight <arinzelight2@gmail.com>
2024-01-05 17:21:56 -0800

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Make a PUT request to 0.0.0.0:5000/catch_me with a custom User-Agent header
-curl -sX PUT -H "User-Agent: HolbertonSchool" -d "user_id=98" http://0.0.0.0:5000/catch_me
+curl -sX PUT -d 'user_id=98' -H "Origin: HolbertonSchool" http://0.0.0.0:5000/catch_me
 
